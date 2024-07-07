d8113ae3313c55625e7ede8c24e6cdfb0688a366
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-07-07 16:32:59 +0200

    Created decompresser.

--- customcompress/Decompresser.cs
+++ customcompress/Decompresser.cs
@@ -5,12 +5,10 @@ namespace customcompress;
 public class Decompresser
 {
     private string _compressedFilePath;
-    private string _decompressedFilePath;
     
     public Decompresser()
     {
         _compressedFilePath = String.Empty;
-        _decompressedFilePath = String.Empty;
     }
     
     public void GetFile(string compressedFilePath)
@@ -20,13 +18,36 @@ public class Decompresser
     
     public bool DecompressFile()
     {
+        FileStream file = File.OpenRead(_compressedFilePath);
+        
+        string text = File.ReadAllText(_compressedFilePath);
+        
+        string decompressedText = String.Empty;
+        
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (Char.IsDigit(text[i]))
+            {
+                int count = Int32.Parse(text[i].ToString());
+                for (int j = 0; j < count; j++)
+                {
+                    decompressedText += text[i-1];
+                }
+            }
+        }
+        
+        string nameOfFile = file.Name.Split(".")[0].Split("/")[^1];
+        
+        Console.WriteLine(decompressedText);
+        
+        CreateDecompressedFile(decompressedText, nameOfFile + ".txt");
         
         return true;
     }
     
-    public bool CreateDecompressedFile()
+    public bool CreateDecompressedFile(string decompressedText, string name)
     {
-        
+        File.WriteAllText(name, decompressedText);
         return true;
     }
 }
